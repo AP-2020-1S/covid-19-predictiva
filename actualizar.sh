8e0f8f536c26ebaf461e71bec5027a5101aa4162
yoalvarezh <yoalvarezh@unal.edu.co>
2020-09-01 21:00:40 -0500

    Create actualizar.sh

---
+++ actualizar.sh
@@ -0,0 +1,27 @@
+#python3.7 covid-19-predictiva/Datalab/Cargue_inicial_datos.py
+
+jupyter nbconvert --execute covid-19-predictiva/Datalab/Exploratorio.ipynb
+
+jupyter nbconvert --to html covid-19-predictiva/Datalab/Exploratorio.ipynb
+mv covid-19-predictiva/Datalab/Exploratorio.html covid-19-predictiva/Dashboard/SARS-COV-2/
+
+cd covid-19-predictiva/Dashboard/SARS-COV-2
+python3.7 jinja2_resultados.py
+
+cd ../../..
+
+cp -R covid-19-predictiva/Dashboard/SARS-COV-2 yoalvarezh.github.io
+
+cd yoalvarezh.github.io
+git add .
+git commit -m "Actualización Dashboard"
+git push origin
+
+cd ..
+
+cd covid-19-predictiva
+git add .
+git commit -m "Actualización Dashboard"
+git push origin
+
+cd ..
